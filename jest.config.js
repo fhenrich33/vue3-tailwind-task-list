a6c6cf014148cf7b716b9db5948a62991076e58c
Felipe Henrich <henrich.felipe@gmail.com>
2022-03-11 16:07:30 -0300

    fix: test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,7 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  testMatch: [path.join(__dirname, "*/**/*.@(spec|test).@(js|ts)?(x)")],
-  moduleFileExtensions: ["ts", "js", "vue", "json"],
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-    "^.+\\.vue$": "vue3-jest",
-  },
-  testEnvironment: "jsdom",
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  testMatch: [path.join(__dirname, '*/**/*.@(spec|test).@(js|ts)?(x)')],
 };
