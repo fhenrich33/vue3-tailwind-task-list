85f17119d01528a3fd1b23ae91fcae7a303a2368
Felipe Henrich <henrich.felipe@gmail.com>
2022-03-12 14:09:00 -0300

    infra: update actions to include linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:vue/essential',
-  ],
+  extends: ["plugin:vue/essential"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint'
-  ],
+  plugins: ["vue", "@typescript-eslint"],
   rules: {
-    'no-console': 'warn',
-    'no-debugger': 'warn',
+    "no-console": "warn",
+    "no-debugger": "warn",
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: ['Card'],
+      },
+    ],
   },
-}
+};
