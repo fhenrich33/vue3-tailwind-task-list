ecddf863849401aad1e7934031db61069f4f09cc
Felipe Henrich <henrich.felipe@gmail.com>
2022-03-15 23:34:41 -0300

    chore: exclude E2E from jest runs

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-  testMatch: [path.join(__dirname, '*/**/*.@(spec|test).@(js|ts)?(x)')],
+  testMatch: [path.join(__dirname, "*/**/*.@(spec|test).@(js|ts)?(x)")],
+  testPathIgnorePatterns: ["e2e"],
 };
